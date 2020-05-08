886be99214fd77fe44d9585a703341aba2e15641
Brian Cooke <bric@stripe.com>
2020-05-08 07:37:40 -0700

    Simplify MainVC, use a stackview

--- SwiftAppTemplate/SwiftAppTemplate/MainViewController.swift
+++ SwiftAppTemplate/SwiftAppTemplate/MainViewController.swift
@@ -9,20 +9,12 @@
 import UIKit
 
 class MainViewController: UIViewController, UITextFieldDelegate {
-    private let discoverReadersButton = UIButton()
-    private let collectPaymentButton = UIButton()
     private let paymentTextField = UITextField()
-    private let containerView = UIView()
-    private let loadingIndicator = UIActivityIndicatorView()
-    private let loadingContainerView = UIView()
-    private let loadingLabel = UILabel()
-
     private static let spacing: CGFloat = 50.0
     private static let stripeBlue = UIColor(red: 103.0/255.0, green:  114.0/255.0, blue: 229.0/255.0, alpha: 1.0)
 
     init() {
         super.init(nibName: nil, bundle: nil)
-        paymentTextField.delegate = self
     }
 
     required init?(coder aDecoder: NSCoder) {
@@ -35,107 +27,43 @@ class MainViewController: UIViewController, UITextFieldDelegate {
         title = "Example App"
         view.backgroundColor = UIColor.white
 
+        paymentTextField.delegate = self
+
+        let discoverReadersButton = MainViewController.stripeyButton()
         discoverReadersButton.setTitle("Discover Readers", for: .normal)
-        discoverReadersButton.setTitleColor(UIColor.white, for: .normal)
-        discoverReadersButton.layer.borderWidth = 1.0
-        discoverReadersButton.layer.borderColor = MainViewController.stripeBlue.cgColor
-        discoverReadersButton.layer.cornerRadius = 4.0
-        discoverReadersButton.titleEdgeInsets = UIEdgeInsets(top: 20.0, left: 10.0, bottom: 20.0, right: 10.0)
-        discoverReadersButton.backgroundColor = MainViewController.stripeBlue
         discoverReadersButton.addTarget(self, action: #selector(discoverReader), for: .touchUpInside)
 
-
+        let collectPaymentButton = MainViewController.stripeyButton()
         collectPaymentButton.setTitle("Collect Payment", for: .normal)
-        collectPaymentButton.setTitleColor(UIColor.white, for: .normal)
-        collectPaymentButton.layer.borderWidth = 1.0
-        collectPaymentButton.layer.borderColor = MainViewController.stripeBlue.cgColor
-        collectPaymentButton.layer.cornerRadius = 4.0
-        collectPaymentButton.titleEdgeInsets = UIEdgeInsets(top: 20.0, left: 10.0, bottom: 20.0, right: 10.0)
-        collectPaymentButton.backgroundColor = MainViewController.stripeBlue
         collectPaymentButton.addTarget(self, action: #selector(collectPayment), for: .touchUpInside)
 
         paymentTextField.placeholder = "0"
-        paymentTextField.keyboardType = .numbersAndPunctuation
+        paymentTextField.keyboardType = .numberPad
         paymentTextField.borderStyle = .roundedRect
 
-        paymentTextField.leftView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 10.0, height: 0.0))
-        paymentTextField.leftViewMode = .always
-
-        loadingContainerView.isHidden = true
-        loadingContainerView.backgroundColor = UIColor(white: 0.0, alpha: 0.75)
-
-        loadingLabel.textColor = UIColor.white
-
-        view.addSubview(containerView)
-        containerView.addSubview(discoverReadersButton)
-        containerView.addSubview(collectPaymentButton)
-        containerView.addSubview(paymentTextField)
-        view.addSubview(loadingContainerView)
-        loadingContainerView.addSubview(loadingIndicator)
-        loadingContainerView.addSubview(loadingLabel)
-
-        containerView.translatesAutoresizingMaskIntoConstraints = false;
-        NSLayoutConstraint.activate([
-            containerView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -MainViewController.spacing),
-            containerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            containerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        let stackView = UIStackView(arrangedSubviews: [
+            UIView(), // Spacer()
+            discoverReadersButton,
+            paymentTextField,
+            collectPaymentButton,
+            UIView(), // Spacer()
         ])
+        stackView.axis = .vertical
+        stackView.spacing = MainViewController.spacing
+        stackView.isLayoutMarginsRelativeArrangement = true
+        view.addSubview(stackView)
 
-        discoverReadersButton.translatesAutoresizingMaskIntoConstraints = false;
+        stackView.translatesAutoresizingMaskIntoConstraints = false;
         NSLayoutConstraint.activate([
-            discoverReadersButton.topAnchor.constraint(equalTo: containerView.topAnchor),
-            discoverReadersButton.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
-            discoverReadersButton.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: MainViewController.spacing),
-            discoverReadersButton.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -MainViewController.spacing),
-        ])
-
-        paymentTextField.translatesAutoresizingMaskIntoConstraints = false;
-        NSLayoutConstraint.activate([
-            paymentTextField.topAnchor.constraint(equalTo: discoverReadersButton.bottomAnchor, constant: MainViewController.spacing),
-            paymentTextField.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
-            paymentTextField.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: MainViewController.spacing),
-            paymentTextField.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -MainViewController.spacing),
-            paymentTextField.heightAnchor.constraint(equalTo: discoverReadersButton.heightAnchor),
-        ])
-
-
-        collectPaymentButton.translatesAutoresizingMaskIntoConstraints = false;
-        NSLayoutConstraint.activate([
-            collectPaymentButton.topAnchor.constraint(equalTo: paymentTextField.bottomAnchor, constant: MainViewController.spacing),
-            collectPaymentButton.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
-            collectPaymentButton.bottomAnchor.constraint(equalTo: containerView.bottomAnchor),
-            collectPaymentButton.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: MainViewController.spacing),
-            collectPaymentButton.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -MainViewController.spacing),
-        ])
-
-        loadingContainerView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            loadingContainerView.topAnchor.constraint(equalTo: view.topAnchor),
-            loadingContainerView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            loadingContainerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            loadingContainerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-        ])
-
-        loadingIndicator.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            loadingIndicator.centerXAnchor.constraint(equalTo: loadingContainerView.centerXAnchor),
-            loadingIndicator.centerYAnchor.constraint(equalTo: loadingContainerView.centerYAnchor),
-        ])
-
-        loadingLabel.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            loadingLabel.topAnchor.constraint(equalTo: loadingIndicator.bottomAnchor, constant: MainViewController.spacing),
-            loadingLabel.centerXAnchor.constraint(equalTo: loadingContainerView.centerXAnchor)
+            stackView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            stackView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -MainViewController.spacing),
+            stackView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            stackView.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: MainViewController.spacing),
         ])
     }
 
-
     @objc func discoverReader(sender: UIButton) {
-        // The following can be using to show a loading screen and message.
-        // loadingLabel.text = ""
-        // loadingContainerView.isHidden = false
-        // loadingIndicator.startAnimating()
-
+        // Use the DiscoveryVC to start discovery and connect to a reader (or return a reader to connect to)
         let discoveryViewController = DiscoveryViewController()
         navigationController?.pushViewController(discoveryViewController, animated: true)
     }
@@ -163,6 +91,16 @@ class MainViewController: UIViewController, UITextFieldDelegate {
             presentErrorAlert("Please enter valid integer.")
             return
         }
+    }
 
+    private static func stripeyButton() -> UIButton {
+        let button = UIButton()
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.layer.borderWidth = 1.0
+        button.layer.borderColor = MainViewController.stripeBlue.cgColor
+        button.layer.cornerRadius = 4.0
+        button.titleEdgeInsets = UIEdgeInsets(top: 20.0, left: 10.0, bottom: 20.0, right: 10.0)
+        button.backgroundColor = MainViewController.stripeBlue
+        return button
     }
 }
